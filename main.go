be92535272ded94323566aecb57ae1dbdffd12f9
Joseap1996 <joseap1996@live.com>
2026-06-22 14:09:40 -0500

    B: added handlers that track hits to the server, and that can reset the count to 0. Also added apiConfig struct

--- main.go
+++ main.go
@@ -3,14 +3,54 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func handleEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handleHits(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	str := fmt.Sprintf("Hits: %v", hits)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(str))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleHitsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits reset to 0."))
+
+}
+
 func main() {
+	apiCon := apiConfig{}
 	serveMux := http.NewServeMux()
 	serveStruct := http.Server{}
 	serveStruct.Addr = ":8080"
 	serveStruct.Handler = serveMux
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", apiCon.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+
+	// handle registers
+	serveMux.HandleFunc("/healthz", handleEndpoint)
+	serveMux.HandleFunc("/metrics", apiCon.handleHits)
+	serveMux.HandleFunc("/reset", apiCon.handleHitsReset)
 
 	if err := serveStruct.ListenAndServe(); err != nil {
 		fmt.Println(err)
